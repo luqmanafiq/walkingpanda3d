98eebd69980345eceb058b329b16f61363591b95
USER <luqmanafiq135.la@gmail.com>
2022-10-18 10:13:05 +0100

    moving panda

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -3,14 +3,13 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from panda3d.core import ColorAttrib
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
-from panda3d.core import AudioSound
-
 
 
 class WalkingPanda(ShowBase):
-    def __init__(self, no_rotate=False):
+    def __init__(self, no_rotate=False, moon_walk=False):
         ShowBase.__init__(self)
 
         # Load the environment model.
@@ -32,6 +31,29 @@ class WalkingPanda(ShowBase):
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
         self.pandaActor.loop("walk")
+            # Create the four lerp intervals needed for the panda to
+            # walk back and forth.
+        posInterval1 = self.pandaActor.posInterval(13,
+                                                       Point3(0, -10, 0),
+                                                       startPos=Point3(0, 10, 0))
+        posInterval2 = self.pandaActor.posInterval(13,
+                                                       Point3(0, 10, 0),
+                                                       startPos=Point3(0, -10, 0))
+        hprInterval1 = self.pandaActor.hprInterval(3,
+                                                       Point3(360, 0, 0),
+                                                       startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(3,
+                                                       Point3(0, 0, 0),
+                                                       startHpr=Point3(180, 0, 0))
+
+            # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(posInterval1, hprInterval1,
+                                      posInterval2, hprInterval2,
+                                      name="pandaPace")
+        self.pandaPace.loop()
+
+    def setColor(self, task):
+        myNodePath.setColor(banana)
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
