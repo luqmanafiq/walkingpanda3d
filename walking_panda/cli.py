4573c26743a127f35c878d3ec7e6ef0abbf6d210
USER <luqmanafiq135.la@gmail.com>
2022-10-20 23:10:06 +0100

    3d panda with 5 arg parse

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -6,6 +6,14 @@ def cli():
     parser = argparse.ArgumentParser(prog="walking_panda")
     parser.add_argument("--no-rotate",help="Suppress Rotation",
                         action="store_true")
+    parser.add_argument("--scale=1",help="Suppress Scale",
+                        action="store_true")
+    parser.add_argument("--playsound",help="Suppress Sound",
+                        action="store_true")
+    parser.add_argument("--moonwalk", help="Suppress Walking",
+                        action="store_true")
+    parser.add_argument("--spin", help="Suppress Spinning",
+                        action="store_true")
     args = parser.parse_args()
 
     walking = panda.WalkingPanda(**vars(args))
--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -3,18 +3,20 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
-from panda3d.core import ColorAttrib
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
 
 
 class WalkingPanda(ShowBase):
-    def __init__(self, no_rotate=False, moon_walk=False, scale=1):
+    def __init__(self, no_rotate=False, scale=1, playsound=False, moonwalk=False, spin=False):
         ShowBase.__init__(self)
 
         #playsound
-        mySound = self.loader.loadSfx("walking_panda/sound/Defending-the-Princess-Haunted_v002.mp3")
-        mySound.play()
+        playsound = self.loader.loadSfx("walking_panda/sound/Defending-the-Princess-Haunted_v002.mp3")
+        playsound.play()
+
+        if playsound:
+            self.loader.loadSfx("walking_panda/sound/Defending-the-Princess-Haunted_v002.mp3")
 
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
@@ -22,6 +24,7 @@ class WalkingPanda(ShowBase):
         self.scene.reparentTo(self.render)
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
+
         self.scene.setPos(-8, 42, 0)
 
         # Add the spinCameraTask procedure to the task manager.
@@ -30,39 +33,47 @@ class WalkingPanda(ShowBase):
         else:
             self.taskMgr.add(self.DspinCameraTask, "SpinCameraTask")
 
+
         # Load and transform the panda actor.
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
+
         if scale:
             self.pandaActor.setScale(0.005, 0.005, 0.005)
 
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
-        if moon_walk:
-            self.pandaActor.loop("walk")
-            # Create the four lerp intervals needed for the panda to
-            # walk back and forth.
-        posInterval1 = self.pandaActor.posInterval(13,
-                                                       Point3(0, -10, 0),
-                                                       startPos=Point3(0, 10, 0))
-        posInterval2 = self.pandaActor.posInterval(13,
-                                                       Point3(0, 10, 0),
-                                                       startPos=Point3(0, -10, 0))
-        hprInterval1 = self.pandaActor.hprInterval(3,
-                                                       Point3(360, 0, 0),
-                                                       startHpr=Point3(0, 0, 0))
-        hprInterval2 = self.pandaActor.hprInterval(3,
-                                                       Point3(0, 0, 0),
-                                                       startHpr=Point3(180, 0, 0))
+        self.pandaActor.loop("walk")
 
-            # Create and play the sequence that coordinates the intervals.
+
+        # Create the four lerp intervals needed for the panda to
+        # walk back and forth.
+        # It can spin 10 times at the end and 2 times at the start
+        if spin:
+            self.pandaActor.hprInterval
+        posInterval1 = self.pandaActor.posInterval(13,
+                                                   Point3(0, -10, 0),
+                                                   startPos=Point3(0, 10, 0))
+        posInterval2 = self.pandaActor.posInterval(13,
+                                                   Point3(0, 10, 0),
+                                                   startPos=Point3(0, -10, 0))
+        hprInterval1 = self.pandaActor.hprInterval(3,
+                                                   Point3(3600, 0, 0),
+                                                   startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(3,
+                                                   Point3(0, 0, 0),
+                                                   startHpr=Point3(360, 0, 0))
+
+        # Create and play the sequence that coordinates the intervals.
+        #Panda moonwalk at every return. It will walk straight first then moonwalk because He is cool :)
+        if moonwalk:
+            self.pandaPace
+
         self.pandaPace = Sequence(posInterval1, hprInterval1,
-                                      posInterval2, hprInterval2,
-                                      name="pandaPace")
+                                  posInterval2, hprInterval2,
+                                  name="pandaPace")
         self.pandaPace.loop()
 
-    def setColor(self, task):
-        myNodePath.setColor(1, 0, 1, 0)
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
@@ -78,6 +89,7 @@ class WalkingPanda(ShowBase):
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
         return task.cont
+
 
 walking = WalkingPanda()
 walking.run()
